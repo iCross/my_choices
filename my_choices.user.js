919349352c6089f54abe78edf41a210894502f69
Lukas <rja@icross.cc>
2023-04-02 16:27:25 +0800

    refactor: set width to auto-adjust to text size

--- my_choices.user.js
+++ my_choices.user.js
@@ -1,9 +1,13 @@
 // ==UserScript==
 // @name         My Choices
 // @namespace    https://github.com/iCross
-// @version      0.1
+// @version      0.1.1
 // @description  Replace default select menus with Choices.js
+// @icon         https://raw.githubusercontent.com/iCross/my_choices/main/icon.jpg
 // @author       https://github.com/iCross
+// @homepageURL  https://github.com/iCross/my_choices
+// @supportURL   https://github.com/iCross/my_choices/issues
+// @downloadURL  https://github.com/iCross/my_choices/raw/main/my_choices.user.js
 // @match        *://*/*
 // @grant        GM_registerMenuCommand
 // @require      https://cdn.jsdelivr.net/npm/choices.js/public/assets/scripts/choices.min.js
@@ -13,15 +17,25 @@
 (function () {
   "use strict";
 
-  console.log("Userscript is running.");
-
+  // console.log("Userscript is running.");
   // Add Choices.js CSS to the document
   function addChoicesCSS() {
     const choicesCSSLink = document.createElement("link");
-    choicesCSSLink.href = "https://cdn.jsdelivr.net/npm/choices.js@9.0.1/public/assets/styles/choices.min.css";
+    choicesCSSLink.href =
+      "https://cdn.jsdelivr.net/npm/choices.js@9.0.1/public/assets/styles/choices.min.css";
     choicesCSSLink.rel = "stylesheet";
     console.log("Adding Choices.js CSS");
     document.head.appendChild(choicesCSSLink);
+
+    const customStyle = document.createElement("style");
+    customStyle.textContent = `
+    .choices__list--dropdown,
+    .choices__list[aria-expanded] {
+      word-break: break-word;
+      width: max-content;
+    }
+  `;
+    document.head.appendChild(customStyle);
   }
 
   // Replace default select menus with Choices.js
@@ -30,14 +44,18 @@
 
     document.querySelectorAll("select").forEach(function (select) {
       if (!select._choicesInstance) {
-        new Choices(select);
+        // Create a new Choices instance with the 'itemSelectText' set to an empty string
+
+        new Choices(select, {
+          itemSelectText: "",
+        });
       }
     });
   }
 
   const { register } = VM.shortcut;
 
-  register("c-e", () => {
+  register("c-i", () => {
     console.log("You just pressed Shortcut in violetmonkey.");
 
     // Add Choices.js CSS when the keyboard shortcut is pressed
@@ -46,7 +64,6 @@
     // Run once when the script is loaded
     applyChoices();
   });
-
 
   // Register a menu command to trigger the same function
   GM_registerMenuCommand("Apply Choices.js", () => {
